4c3f6ff3ea0ae6c2ebea0e558968bc65e586cef2
João Araújo <61572306+joaoparaujocr@users.noreply.github.com>
2022-12-03 13:11:22 -0500

    Initial commit

---
+++ animals/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Animal(models.Model):
+    name = models.CharField(max_length=255)
+    sound = models.CharField(max_length=255)
+    
+    def speak(self):
+        return f'The {self.name} says "{self.sound}"'

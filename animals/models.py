1f75e49260c146ad6b82f32a1488ada89f788be7
joaoparaujocr <araujo.joao.paulo.jaa@gmail.com>
2022-12-03 14:43:31 -0400

    code testscase

--- animals/models.py
+++ animals/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
+
 class Animal(models.Model):
     name = models.CharField(max_length=255)
     sound = models.CharField(max_length=255)
-    
+
     def speak(self):
         return f'The {self.name} says "{self.sound}"'
